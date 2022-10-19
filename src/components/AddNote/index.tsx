432418bc47dfcbfa4828fa73e607bf07c1a0b6a1
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-10-19 00:31:39 -0300

    title e text area

--- src/components/AddNote/index.tsx
+++ src/components/AddNote/index.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import { FiCheckSquare } from "react-icons/fi";
+
 export function AddNote() {
-    return (
-        <div>
-            <input type="text"/>
-        </div>
-    )
+  const [title, setTitle] = useState("");
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <FiCheckSquare />
+      </div>
+      <div>
+        <textarea name="text"></textarea>
+      </div>
+    </div>
+  );
 }
